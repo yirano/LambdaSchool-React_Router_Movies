09d78bb84f88ff40042b0f061c65507b923c9765
yirano <yirano@protonmail.ch>
2020-03-06 22:04:46 -0500

    Updates

---
+++ precache-manifest.ed2d6b39e5f95a2f93840c679e1e8efa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05049a80e57509269aab4035de92bd06",
+    "url": "/React-Router-Movies/index.html"
+  },
+  {
+    "revision": "bbee84af9637c74228ce",
+    "url": "/React-Router-Movies/static/css/main.b6bf4a22.chunk.css"
+  },
+  {
+    "revision": "cd6215b5dda8d2488cb9",
+    "url": "/React-Router-Movies/static/js/2.d7f1bcc4.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/React-Router-Movies/static/js/2.d7f1bcc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbee84af9637c74228ce",
+    "url": "/React-Router-Movies/static/js/main.61f4711c.chunk.js"
+  },
+  {
+    "revision": "4d9368f93c597b589835",
+    "url": "/React-Router-Movies/static/js/runtime-main.90f1861e.js"
+  }
+]);
