522000c6cf53da2654a470f756740804e93b80db
yirano <yirano@protonmail.ch>
2020-04-22 20:15:43 -0400

    Meet MVP and create MovieCard component to reuse in MovieList and Movie component

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,26 @@
 import React, { useState } from 'react';
 
 import SavedList from './Movies/SavedList';
+import { Route } from 'react-router-dom';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 
 const App = () => {
-  const [savedList, setSavedList] = useState( [] );
+  const [savedList, setSavedList] = useState([]);
 
   const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
+    setSavedList([...savedList, movie]);
   };
 
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Route exact path="/">
+        <MovieList />
+      </Route>
+      <Route exact path="/movies/:id">
+        <Movie />
+      </Route>
     </div>
   );
 };
